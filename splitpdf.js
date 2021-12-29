67303e11480907d540ad214e0d8361ef5a5208df
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2021-12-30 00:13:39 +0530

    check is everything okay

--- splitpdf.js
+++ splitpdf.js
@@ -1,11 +1,23 @@
 const { PageSizes, PDFDocument } = require('pdf-lib')
 const fetch = require('node-fetch');
+var fs = require('fs');
 
 (async () => {
-  const url = 'https://brokerloopdev.s3.amazonaws.com/orders/dbcb3cf1-d537-4969-bce0-8d1a8f144ef8/export.pdf'
+  try {
+    const url = 'https://brokerloopdev.s3.amazonaws.com/orders/dbcb3cf1-d537-4969-bce0-8d1a8f144ef8/export.pdf'
   const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
 
   // Load a PDFDocument from the existing PDF bytes
   const pdfDoc = await PDFDocument.load(existingPdfBytes)
   const pdfDocnew = await PDFDocument.create()
+  const [existingPage] = await pdfDocnew.copyPages(pdfDoc, [1])
+  pdfDocnew.addPage(existingPage)
+
+  const pdfBytes = await pdfDocnew.save()
+  fs.writeFileSync('./test1.pdf', pdfBytes);
+  } catch (e) {
+    console.log(e);
+  }
+  
+
 })()
